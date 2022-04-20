616c6dcf75899a4745562764445c6dd34a1c4bf4
Jesus Morales Jr <jmjles@gmail.com>
2022-04-19 18:11:00 -0700

    added endpoints

--- utils/axios.js
+++ utils/axios.js
@@ -2,44 +2,131 @@ import axios from 'axios'
 const url = 'https://brud-new-1.herokuapp.com/api'
 
 export const api = axios.create({
-    baseURL: url,
+  baseURL: url,
 })
 
 /***********************
  * Restaurant endpoint
  ***********************/
 export const restaurantApi = axios.create({
-    baseURL: `${url}/restaurant`,
+  baseURL: `${url}/restaurant`,
 })
 
 export const login = ({ email, password, deviceToken }) => {
-    try {
-        return ({ data } = restaurantApi.post('/login', {
-            email,
-            password,
-            device_token: deviceToken,
-        }))
-    } catch (er) {
-        return er
-    }
+  try {
+    return restaurantApi.post('/login', {
+      email,
+      password,
+      device_token: deviceToken,
+    })
+  } catch (er) {
+    return er
+  }
+}
+export const signUp = ({ email }) => {
+  try {
+    return restaurantApi.post('/signup', {
+      email,
+    })
+  } catch (er) {
+    return er
+  }
+}
+export const otpCheck = ({ id, otp }) => {
+  try {
+    return restaurantApi.post('/otp-check', {
+      id,
+      otp,
+    })
+  } catch (er) {
+    return er
+  }
+}
+
+export const resendOtp = ({ id, otp }) => {
+  try {
+    return restaurantApi.post('/resend-otp', {
+      id,
+    })
+  } catch (er) {
+    return er
+  }
+}
+
+export const resetPassword = ({ id, newPassword, confirmPassword }) => {
+  try {
+    return restaurantApi.post('/reset-password', {
+      id,
+      new_password: newPassword,
+      confirm_password: confirmPassword,
+    })
+  } catch (er) {
+    return er
+  }
+}
+
+export const setupCafe = ({
+  id,
+  restaurantName,
+  managerName,
+  countryCode = 1,
+  phone,
+  fullAddress,
+  city,
+  state,
+  latitude,
+  longitude,
+  image = '',
+  images = [],
+  about = '',
+}) => {
+  try {
+    return restaurantApi.post('/setup-cafe', {
+      id,
+      restaurant_name: restaurantName,
+      manager_name: managerName,
+      country_code: countryCode,
+      phone_number: phone,
+      full_address: fullAddress,
+      city,
+      state,
+      lattitude: latitude,
+      longitude,
+      image,
+      images,
+      about,
+    })
+  } catch (er) {
+    return er
+  }
+}
+
+export const editCafe = ({ id }) => {
+  try {
+    return restaurantApi.post('/setup-cafe', {
+      id,
+    })
+  } catch (er) {
+    return er
+  }
 }
 /*****************
  * Cafe endpoint
  *****************/
 export const cafeApi = axios.create({
-    baseURL: `${url}/user`,
+  baseURL: `${url}/user`,
 })
 
 /******************
  * Order endpoint
  ******************/
 export const orderApi = axios.create({
-    baseURL: `${url}/order`,
+  baseURL: `${url}/order`,
 })
 
 /*****************
  * Card endpoint
  *****************/
 export const cardApi = axios.create({
-    baseURL: `${url}/card`,
+  baseURL: `${url}/card`,
 })
