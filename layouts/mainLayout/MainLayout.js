02031448b237e353dba89dba596d595b7031e4d8
Jesus Morales Jr <jmjles@gmail.com>
2022-04-23 13:37:55 -0700

    added styling

--- layouts/mainLayout/MainLayout.js
+++ layouts/mainLayout/MainLayout.js
@@ -11,7 +11,7 @@ const MainLayout = ({
     ...props
 }) => {
     return (
-        <div>
+        <div className='MainLayout'>
             <Head>
                 <title>Brud Rewards {title ? `| ${title}` : ''}</title>
                 <meta name='description' content={description} />
