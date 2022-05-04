94e36b3b0f05e851931ae942a53528cf3afc9d56
Roop Hayer <ruphayer@gmail.com>
2022-05-03 20:25:01 -0700

    added placeholder pic for coffee

--- utils/images.js
+++ utils/images.js
@@ -10,6 +10,7 @@ import earn2 from '../public/assets/Earn1.jpg'
 import coffee from '../public/assets/Coffee.png'
 import coffee2 from '../public/assets/Coffee2.png'
 import coffee3 from '../public/assets/Coffee3.png'
+import coffeePlaceholder from '../public/assets/coffeePlaceholder.jpg'
 import cake from '../public/assets/Cake.png'
 import background from '../public/assets/background.jpeg'
 import background2 from '../public/assets/background.png'
@@ -29,41 +30,42 @@ import roop from '../public/assets/team/Roop.png'
 import shayan from '../public/assets/team/Shayan.png'
 
 class Image {
-  constructor(src, alt = '') {
-    this.src = src
-    this.alt = alt
-  }
+    constructor(src, alt = '') {
+        this.src = src
+        this.alt = alt
+    }
 }
 
 const images = {
-  rewards: new Image(rewards, "Bru'd rewards Logo"),
-  rewardsInverted: new Image(invertedRewards, "Bru'd rewards Logo"),
-  redeem: new Image(redeem, 'Redeem'),
-  redeem2: new Image(redeem2, 'Redeem'),
-  logo: new Image(logo, "Bru'd rewards Logo"),
-  experience: new Image(experience, 'Happy Face'),
-  experience2: new Image(experience2, 'Happy Face'),
-  earn: new Image(earn, 'Earn'),
-  earn2: new Image(earn2, 'Earn'),
-  coffee: new Image(coffee, 'Coffee'),
-  coffee2: new Image(coffee2, 'Three Coffee Cups'),
-  coffee3: new Image(coffee3, 'Pouring Coffee'),
-  cake: new Image(cake, 'Chocolate Cake'),
-  background: new Image(background, 'Coffee Wallpaper'),
-  background2: new Image(background2, 'Coffee Splash'),
-  shopOutline: new Image(shopOutline, 'Shop'),
-  noFood: new Image(noFood, 'No Food Available'),
-  closedOpen: new Image(closedOpen, 'Closed/Open Sign'),
-  fastFood: new Image(fastFood, 'Fast Food'),
-  cheeseOutline: new Image(cheeseOutline, 'Cheese'),
-  lockedOutline: new Image(lockedOutline, 'Locked'),
-  approvedOutline: new Image(approvedOutline, 'Approved'),
-  holdingLatte: new Image(holdingLatte, 'Women Holding Hot Latte'),
-  doron: new Image(doron, 'Doron Portrait'),
-  harini: new Image(harini, 'Harini Portrait'),
-  jesus: new Image(jesus, 'Jesus Portrait'),
-  roop: new Image(roop, 'Roop Portrait'),
-  shayan: new Image(shayan, 'Shayan Portrait'),
+    rewards: new Image(rewards, "Bru'd rewards Logo"),
+    rewardsInverted: new Image(invertedRewards, "Bru'd rewards Logo"),
+    redeem: new Image(redeem, 'Redeem'),
+    redeem2: new Image(redeem2, 'Redeem'),
+    logo: new Image(logo, "Bru'd rewards Logo"),
+    experience: new Image(experience, 'Happy Face'),
+    experience2: new Image(experience2, 'Happy Face'),
+    earn: new Image(earn, 'Earn'),
+    earn2: new Image(earn2, 'Earn'),
+    coffee: new Image(coffee, 'Coffee'),
+    coffee2: new Image(coffee2, 'Three Coffee Cups'),
+    coffee3: new Image(coffee3, 'Pouring Coffee'),
+    coffeePlaceholder: new Image(coffeePlaceholder, 'Placeholder Coffee'),
+    cake: new Image(cake, 'Chocolate Cake'),
+    background: new Image(background, 'Coffee Wallpaper'),
+    background2: new Image(background2, 'Coffee Splash'),
+    shopOutline: new Image(shopOutline, 'Shop'),
+    noFood: new Image(noFood, 'No Food Available'),
+    closedOpen: new Image(closedOpen, 'Closed/Open Sign'),
+    fastFood: new Image(fastFood, 'Fast Food'),
+    cheeseOutline: new Image(cheeseOutline, 'Cheese'),
+    lockedOutline: new Image(lockedOutline, 'Locked'),
+    approvedOutline: new Image(approvedOutline, 'Approved'),
+    holdingLatte: new Image(holdingLatte, 'Women Holding Hot Latte'),
+    doron: new Image(doron, 'Doron Portrait'),
+    harini: new Image(harini, 'Harini Portrait'),
+    jesus: new Image(jesus, 'Jesus Portrait'),
+    roop: new Image(roop, 'Roop Portrait'),
+    shayan: new Image(shayan, 'Shayan Portrait'),
 }
 
 export default images
