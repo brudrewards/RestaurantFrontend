98c07260026e14d3a5f8b3989b57f2f22f308495
Shayan Valaie <valaieshayan1@gmail.com>
2022-06-01 13:30:48 -0700

    delete modal added

--- pages/modifier.js
+++ pages/modifier.js
@@ -8,17 +8,38 @@ import ModifierCard from '../layouts/components/modifier/modifierCard'
 import modifierCard1 from '../public/assets/modifierCard1.png'
 import modifierCard2 from '../public/assets/modifierCard2.png'
 import modifierCard3 from '../public/assets/modifierCard3.png'
+import NoFood from '../public/assets/noFood.png'
 import { Typography } from '@mui/material';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import { useForm } from "react-hook-form";
 import MenuItem from '@mui/material/MenuItem';
+import * as FiIcons from 'react-icons/fi'
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+import Image from 'next/image'
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 const Modifier = () => {
     const [size, setSize] = React.useState('');
-    const [milk, setMilk] = React.useState('');
-    const [extra, setExtra] = React.useState('');
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
 
 
 
@@ -28,16 +49,7 @@ const Modifier = () => {
 
     };
 
-    const handleMilk = (event) => {
-        setMilk(event.target.value);
-        console.log(Milk)
 
-    };
-    const handleExtra = (event) => {
-        setExtra(event.target.value);
-
-
-    };
     const { handleSubmit, register, formState: { errors } } = useForm();
     const onSubmit = values => console.log(values);
 
@@ -70,10 +82,10 @@ const Modifier = () => {
 
             <Typography variant="h6">Modifier</Typography>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <Grid sx={{ marginTop: 3 }} container>
+                <Grid sx={{ marginTop: 3 }} container spacing={3}>
 
 
-                    <Grid item xs={12}>
+                    <Grid item xs={8} md={9} lg={9}>
                         <FormControl variant="standard" sx={{ minWidth: "100%" }}>
                             <InputLabel id="demo-simple-select-standard-label">Size</InputLabel>
                             <Select {
@@ -98,80 +110,59 @@ const Modifier = () => {
                     </Grid>
 
 
-                </Grid>
-
-
-
-                <Grid sx={{ marginTop: 3 }} container>
-
-
-                    <Grid item xs={12}>
-                        <FormControl variant="standard" sx={{ minWidth: "100%" }}>
-                            <InputLabel id="demo-simple-select-standard-label">Milk</InputLabel>
-                            <Select {
-                                ...register("milk", {
-                                    required: true
-                                })
-                            }
-                                labelId="demo-simple-select-standard-label"
-                                id="demo-simple-select-standard"
-                                value={milk}
-                                onChange={handleMilk}
-                                label="milk"
-                            >
-
-                                <MenuItem value="oat">Oat</MenuItem>
-                                <MenuItem value="almond">Almond</MenuItem>
-                                <MenuItem value="skim">Skim</MenuItem>
-                            </Select>
-                            <Typography sx={{ color: '#D35E3C' }} variant="body2">{errors.milk && <span>Please Input milk</span>}</Typography>
-                        </FormControl>
+                    <Grid sx={{ marginTop: 2 }} item xs={1} md={1} lg={1}>
+                        <Button variant="rounded">Edit</Button>
+                    </Grid>
+                    <Grid sx={{
+                        marginTop: 2, marginLeft: {
+                            xs: 5,
+                            md: 1,
+                            lg: 1
+                        }
+                    }} item xs={1} md={1} lg={1}>
+                        <Button onClick={handleOpen}><FiIcons.FiTrash style={{ width: '25px', height: '25px' }} /></Button>
 
                     </Grid>
 
 
-                </Grid>
-
-
-
-
-
-
-                <Grid sx={{ marginTop: 3 }} container>
-
-
-                    <Grid item xs={12}>
-                        <FormControl variant="standard" sx={{ minWidth: "100%" }}>
-                            <InputLabel id="demo-simple-select-standard-label">Extras</InputLabel>
-                            <Select {
-                                ...register("extra", {
-                                    required: true
-                                })
-                            }
-                                labelId="demo-simple-select-standard-label"
-                                id="demo-simple-select-standard"
-                                value={extra}
-                                onChange={handleExtra}
-                                label="size"
-                            >
-
-                                <MenuItem value="1">Extra1</MenuItem>
-                                <MenuItem value="2">Extra2</MenuItem>
-                                <MenuItem value="3">Extra3</MenuItem>
-                            </Select>
-                            <Typography sx={{ color: '#D35E3C' }} variant="body2">{errors.extra && <span>Please Input Extra</span>}</Typography>
-                        </FormControl>
-
-                    </Grid>
-
 
                 </Grid>
-                <Grid sx={{ marginTop: 5 }} container>
-                    <Grid sx={{ marginLeft: 'auto', marginRight: 'auto' }} item xs={12} md={6}>
-                        <Button fullWidth={true} type="submit" variant="rounded">Add Modifier</Button>
 
-                    </Grid>
-                </Grid>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Box sx={{ marginLeft: "auto", marginRight: 'auto' }}>
+                            <Image src={NoFood}
+                                width={350}
+                                height={350}
+                                priority={true}
+
+                            />
+                            <Typography variant="p">
+                                Are you sure you want to delete the modifier?
+                            </Typography>
+
+                            <Box sx={{ display: 'flex', justifyContent: 'space-around', marginTop: 2 }}>
+                                <Button onClick={handleClose} sx={{ backgroundColor: '#E5E5E5', color: 'black' }} variant="rounded">Cancel</Button>
+                                <Button variant="rounded">Delete</Button>
+                            </Box>
+                        </Box>
+
+                    </Box>
+                </Modal>
+
+
+
+
+
+
+
+
+
             </form>
 
 
